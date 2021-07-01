91879feb2bef6fb65bd6b2ad868e50e0f479583f
anguyen698 <63482191+anguyen698@users.noreply.github.com>
2021-06-30 19:03:08 -0700

    Add POST parking_lot route

---
+++ routes/parking_lot.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+const Lots = require("../database/model/lotModel");
+
+/* POST parking_lot route */
+router.post("/parking_lot", (req, res) => {
+});
+
+module.exports = router;
