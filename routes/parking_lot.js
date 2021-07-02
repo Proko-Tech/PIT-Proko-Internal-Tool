b18fd8c510ba18b77d1f4101c6a95b19b72f7183
anguyen698 <63482191+anguyen698@users.noreply.github.com>
2021-07-02 15:38:32 -0700

    Created GET /parking_lot route and retrieved info

--- routes/parking_lot.js
+++ routes/parking_lot.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 const Lots = require("../database/model/lotModel");
 
+/* GET parking_lot route */
+router.get("/parking_lot", (req, res) => {
+    Lots.get();
+});
+
 /* POST parking_lot route */
 router.post("/parking_lot", (req, res) => {
 });
