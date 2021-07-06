67c91eefc31f289e995d40fc6683071a6131b170
Samuel <ngkhsamuel@gmail.com>
2021-07-06 12:58:21 -0700

    Implemented POST route for parking_lot.js:

--- routes/parking_lot.js
+++ routes/parking_lot.js
@@ -5,6 +5,7 @@ const Lots = require("../database/model/lotModel");
 
 /* POST parking_lot route */
 router.post("/parking_lot", (req, res) => {
+      Lots.insert(req.body);
 });
 
 module.exports = router;
