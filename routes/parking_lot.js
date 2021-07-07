e475b87261f2458208baf7efe725866e7cb055ec
Samuel <ngkhsamuel@gmail.com>
2021-07-06 17:10:46 -0700

    Added POST for route/parking_lot.js

--- routes/parking_lot.js
+++ routes/parking_lot.js
@@ -5,7 +5,7 @@ const Lots = require("../database/model/lotModel");
 
 /* POST parking_lot route */
 router.post("/parking_lot", (req, res) => {
-      Lots.insert(req.body);
+      lot_model.insert(req.body);
 });
 
 module.exports = router;
