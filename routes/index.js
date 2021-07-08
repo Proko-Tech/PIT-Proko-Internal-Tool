3217c8e70038894d0f363e9455604467526eadcc
NAIBI YILIDAER <nyilidaer@sdsu.edu>
2021-07-07 17:38:17 -0700

    feature/get_by_adminid

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const lotsModel = require('../database/model/lotModel');
 router.get('/', async function(req, res, next) {
   const result = await lotsModel.get();
   const a = await lotsModel.getByAdminID('1');
-  console.log(a);
+
   res.render('index', { title: 'Express', result });
 
 });
