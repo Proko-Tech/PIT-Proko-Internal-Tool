5179bd0560bbf9c48a6726bf5de06bde64b260b7
NAIBI YILIDAER <nyilidaer@sdsu.edu>
2021-07-14 20:56:40 -0700

    feature/get_new

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,8 @@ router.get('/', async function(req, res, next) {
   const result = await lotsModel.get();
   res.render('index', { title: 'Express', result });
 
-  /* get new page. */
-  router.get('/new', async function(req, res, next) {
-    res.render('../views/page/parkingLot/newParkingLot');
-  });
-
 });
+
 
 
 module.exports = router;
--- routes/parking_lot.js
+++ routes/parking_lot.js
@@ -5,9 +5,10 @@ const lot_model = require("../database/model/lotModel");
 
 
 /* GET new route */
-router.get("/new", function(req, res) {
-    res.render('views/page/parkingLot/newParkingLot');
+router.get('/new', async function(req, res, next) {
+    res.render('page/parkingLot/newParkingLot');
 });
+
 /* GET parking_lot route */
 router.get("/parking_lot", function(req, res) {
     res.end("/parking_lot");
