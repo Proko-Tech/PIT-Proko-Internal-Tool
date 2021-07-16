607812836d579ddd21c6d066b4334e60cf987fd2
anguyen698 <63482191+anguyen698@users.noreply.github.com>
2021-07-15 19:47:32 -0700

    added POST route for user authentication

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 const lotsModel = require('../database/model/lotModel');
+const auth = require('../auth/tokenUtil');
+
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
@@ -12,6 +15,22 @@ router.get('/', async function(req, res, next) {
 
 /* POST / route */
 router.post("/", async function(req, res) {
+  let user_info = {
+    username: req.body.username,
+    password: req.body.password
+  }
+
+  if (user_info.username == process.env.username && 
+      user_info.password == process.env.password) {
+
+    let token = auth.generateToken(user_info);
+
+    if (auth.validateToken(token)) {
+      res.redirect('/');
+    } else {
+      res.redirect('/login');
+    }
+  }
 });
 
 module.exports = router;
