0d1c68d3335e6a5c8701812530e0dc75959baf9c
NAIBI YILIDAER <nyilidaer@sdsu.edu>
2021-07-16 16:19:00 -0700

    feature/pakringlotOwnerships

--- routes/parking_lot.js
+++ routes/parking_lot.js
@@ -2,10 +2,15 @@ var express = require("express");
 var router = express.Router();
 
 const lot_model = require("../database/model/lotModel");
+const lot_ownerships_model = require("../database/model/lotOwnershipsModel");
 
 /* GET new route */
 router.get('/new', async function(req, res, next) {
     res.render('page/parkingLot/newParkingLot');
+});
+/* POST new parking lot */
+router.post("/new", async function (req, res) {
+   await lot_ownerships_model.insert(req.body);
 });
 
 /* GET parking_lot route */
@@ -17,5 +22,7 @@ router.get("/parking_lot", function(req, res) {
 router.post("/parking_lot", async function(req, res) {
     await lot_model.insert(req.body);
 });
+
+
 
 module.exports = router;
