24695324d21cccf1d9706cc63e53cf8bfcbc8b19
anguyen698 <63482191+anguyen698@users.noreply.github.com>
2021-07-26 18:19:58 -0700

    Changed .insert() to .update() based off of id

--- database/model/spotsModel.js
+++ database/model/spotsModel.js
@@ -2,12 +2,14 @@ const db = require('../dbConfig');
 
 /**
  * insert spot hash using the information given to us
+ * @param spot_id
  * @param spot_hash
  */
- async function insertSpotHash (spot_hash){
+ async function insertSpotHash (spot_id, spot_hash){
     try {
         await db('spots')
-            .insert(spot_hash);
+            .where(spot_id)
+            .update(spot_hash);
     } catch (err) {
         console.log(err);
     }
