302c8c00fe5b6f1b649f681a538d7865549d461d
NAIBI YILIDAER <nyilidaer@sdsu.edu>
2021-07-27 17:11:10 -0700

    New param

--- database/model/spotsModel.js
+++ database/model/spotsModel.js
@@ -2,12 +2,12 @@ const db = require('../dbConfig');
 
 /**
  * create spots in the spots table
- * @param lot_id
+ * @param spot_info
  */
-async function create(lot_id){
+async function create(spot_info){
     try {
         await db('spots')
-            .insert([{lot_id : lot_id}]);
+            .insert(spot_info);
     } catch (err) {
         console.log(err);
     }
