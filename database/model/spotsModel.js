f6155549df2ff78602a1e4b2690c02733f407992
anguyen698 <63482191+anguyen698@users.noreply.github.com>
2021-07-29 14:24:24 -0700

    Updated description and placed { } aroung spot_id

--- database/model/spotsModel.js
+++ database/model/spotsModel.js
@@ -1,14 +1,14 @@
 const db = require('../dbConfig');
 
 /**
- * insert spot hash using the information given to us
+ * update spot hash using the information given to us
  * @param spot_id
  * @param spot_hash
  */
  async function update (spot_id, spot_hash){
     try {
         await db('spots')
-            .where(spot_id)
+            .where({ spot_id })
             .update(spot_hash);
     } catch (err) {
         console.log(err);
