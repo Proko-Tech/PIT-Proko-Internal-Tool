795387003180c247634e4e48f4617a74c61b2bc1
YuyusanK <kurinoyuichi@gmail.com>
2021-08-16 01:39:51 -0700

    feat: Complaint system

---
+++ routes/complaints.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+const complaintsModel = require('../database/model/complaintsModel');
+
+require('dotenv').config();
+
+/* GET / complaints page. */
+router.get('/', async function(req, res, next) {
+    const complaints = await complaintsModel.get();
+    res.render('page/complaints/complaints.ejs',{title:"ProkoPark - Complaints Table", complaints});
+});
+
+/* GET / complaint detail page. */
+router.get('/:id', async function(req, res, next) {
+    const id = req.params.id;
+    const complaints = await complaintsModel.getById(id);
+    res.render('page/complaints/complaintDetails.ejs',{title:"ProkoPark - Complaints Detail", complaints, id});
+});
+
+module.exports = router;
