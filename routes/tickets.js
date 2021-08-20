6d0af9358f9d3aa5795b6066641cb6ef384fb2e2
NAIBI YILIDAER <nyilidaer@sdsu.edu>
2021-08-19 21:23:59 -0700

    change file name, modify get fuction, fix assign inspector info card

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-
+var spotModel = require('../database/model/violationsModel')
 /* GET new route */
 router.get('/', async function(req, res, next) {
     res.render('page/tickets/tickets', { title: 'Tickets' });
@@ -9,6 +9,20 @@ router.get('/', async function(req, res, next) {
 /* GET tickets route */
 router.get("/tickets", function(req, res) {
     res.end("/tickets");
+});
+/* GET ticket detail route */
+router.get('/ticket_detail', async function (req,res){
+    //const ticketId = req.query()
+    // const ticketInfo =  //function call ,{ticketInfo}
+    res.render('page/tickets/ticketdetail', {title:" El Fernando Parking - Spot #234"})
+
+});
+/* GET ticket detail route */
+router.get('/:id', async function(req, res, next) {
+    const id = '1';
+    const spot = await spotModel.get(id);
+    console.log(spot);
+    res.render('page/tickets/ticketdetail', {title:" El Fernando Parking - Spot #234", spot, id});
 });
 
 module.exports = router;
