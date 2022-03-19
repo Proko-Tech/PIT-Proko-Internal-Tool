c66a1b011b89f13c6aa52692a575bd935d030ed6
Thomas Li <thomasligreat11@gmail.com>
2022-03-18 23:19:04 -0700

    overhaul sidebar and layout

--- routes/index.js
+++ routes/index.js
@@ -7,32 +7,32 @@ const auth = require('../auth/tokenUtil');
 require('dotenv').config();
 
 /* GET / login page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('page/login/login.ejs');
 });
 
 /* POST / route */
-router.post("/", async function(req, res) {
-  let user_info = req.body
-  
-  if (user_info.username === process.env.USERNAME &&
+router.post("/", async function (req, res) {
+    const user_info = req.body
+
+    if (user_info.username === process.env.USERNAME &&
       user_info.password === process.env.PASSCODE) {
 
-    let token = auth.generateToken(user_info);
-    res.clearCookie('user');
-    res.cookie("user", token);
-    res.redirect('/parking/new');
-  }else{
-    res.status(401).send("Access Denied");
-  }
+        const token = auth.generateToken(user_info);
+        res.clearCookie('user');
+        res.cookie("user", token);
+        res.redirect('/dashboard');
+    } else {
+        res.status(401).send("Access Denied");
+    }
 });
 
 
 /* Logout route */
-router.get('/logout', function(req, res, next) {
-  res.clearCookie('user');
-  console.log("Logged out");
-  res.redirect('/');
+router.get('/logout', function (req, res, next) {
+    res.clearCookie('user');
+    console.log("Logged out");
+    res.redirect('/');
 });
 
 
