0eafcdd253db5817d01fdc183073c13c198a1129
Thomas Li <thomasligreat11@gmail.com>
2022-03-21 20:48:19 -0700

    added handle prediction results page

--- database/model/predictionsModel.js
+++ database/model/predictionsModel.js
@@ -11,4 +11,34 @@ async function getJoinSpots () {
     return result;
 }
 
-module.exports={ getJoinSpots }
+/**
+ * get prediction with spot information by prediction_id.
+ * @param id
+ * @returns {Promise<awaited Knex.QueryBuilder<TRecord, TResult>>}
+ */
+async function getJoinSpotsById (id) {
+    const result = await db('prediction_results')
+        .join('spots', 'prediction_results.spot_secret', 'spots.secret')
+        .select('*')
+        .where('prediction_results.id', id);
+    return result;
+}
+
+/**
+ * update predictions by id.
+ * @param id
+ * @param update_body
+ * @returns {Promise<{status: string}>}
+ */
+async function updateById (id, update_body) {
+    try {
+        await db('prediction_results')
+            .update(update_body)
+            .where({ id });
+        return { status: 'success' };
+    } catch (err) {
+        return { status: 'failed', payload: err };
+    }
+}
+
+module.exports={ getJoinSpots, getJoinSpotsById, updateById }
