e9fc4d3255cb9efd63653194505b503640e9cef5
wehaaaaa <70409209+wehaaaaa@users.noreply.github.com>
2022-09-12 16:45:16 -0700

    add wave chart

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,7 @@
 var express = require('express');
+var moment = require('moment');
 var router = express.Router();
 router.get('/', async function(req, res) {
-    res.render('page/dashboard/dashboard', {title: 'Overview'});
+    res.render('page/dashboard/dashboard', {title:"Overview", moment});
 });
 module.exports = router;
