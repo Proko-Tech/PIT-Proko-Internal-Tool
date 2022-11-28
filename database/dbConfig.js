7ff353e80e556f5cb600d7325a32598239685187
YuyusanK <kurinoyuichi@gmail.com>
2022-11-28 14:32:16 -0800

    fix: remove userModel

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,5 +2,4 @@ const knex = require('knex');
 
 const db = process.env.DB_ENV || 'development'; // Setup for deployment on heroku
 const knexConfig = require('../knexfile.js')[db];
-
 module.exports = knex(knexConfig);
