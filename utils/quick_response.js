83c3a95c609d975756f4d71fcbebeb455266caee
Thomas <thomasligreat11@gmail.com>
2023-01-06 23:53:21 -0800

    made qrcode more compatible with phone scanner

--- utils/quick_response.js
+++ utils/quick_response.js
@@ -7,10 +7,15 @@ const QRCode = require('qrcode');
 async function create(public_key) {
     // if(typeof(public_keys)==="undefined") return undefined;
     const data = "http://web.prokopark.us/spot/pay/"+public_key;
-    // Converting the data into String format
-    const stringData = JSON.stringify(data)
     // Converting the data into base64
-    const base64 = await QRCode.toDataURL(stringData);
+    const base64 = await QRCode.toDataURL(data, {
+        errorCorrectionLevel: 'H',
+        margin : 2,
+        width : 2048,
+        color: {
+            light: '#0000',
+        },
+    });
     return base64;
 }
 
